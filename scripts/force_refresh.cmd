8de35b61fcc14bb42e89a950ee0459aaae2a380f
Marcus-P-114514 <markus_verify@126.com>
2022-04-15 17:54:34 +0800

    Added an external script.

---
+++ scripts/force_refresh.cmd
@@ -0,0 +1,11 @@
+@echo off
+echo ====================
+echo External script
+echo ====================
+echo This is a command line file in order to use some function. We guarantee that this will not hut your system.
+echo If you see this window, just wait till you saw [finished] at the last line, then close it.
+
+TASKKILL /IM wish-recorder.exe /F
+
+echo Will exit now.
+echo [finished]
